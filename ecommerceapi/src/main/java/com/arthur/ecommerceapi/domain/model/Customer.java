14a22efae2c88cea3ad5308c1287f85ec9be1d77
arthurd3 <arthur_camposl@yahoo.com>
2025-07-28 18:51:01 -0300

    adjust mapstruct

--- ecommerceapi/src/main/java/com/arthur/ecommerceapi/domain/model/Customer.java
+++ ecommerceapi/src/main/java/com/arthur/ecommerceapi/domain/model/Customer.java
@@ -1,5 +1,10 @@
 package com.arthur.ecommerceapi.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Customer {
     private Long id;
 
--- ecommerceapi/src/main/java/com/arthur/ecommerceapi/gateways/mapper/CustomerMapper.java
+++ ecommerceapi/src/main/java/com/arthur/ecommerceapi/gateways/mapper/CustomerMapper.java
@@ -1,17 +1,21 @@
 package com.arthur.ecommerceapi.gateways.mapper;
 
 import com.arthur.ecommerceapi.controllers.CustomerController;
+import com.arthur.ecommerceapi.domain.model.Customer;
 import com.arthur.ecommerceapi.dtos.request.CustomerRequest;
 import com.arthur.ecommerceapi.dtos.response.CustomerResponse;
 import com.arthur.ecommerceapi.gateways.entities.CustomerEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "spring")
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING , unmappedTargetPolicy = IGNORE)
 public interface CustomerMapper {
 
-    CustomerController toDomain(CustomerRequest dto);
+    Customer toDomain(CustomerRequest dto);
 
-    CustomerEntity toEntity(CustomerController customerController);
+    CustomerEntity toEntity(Customer customer);
 
-    CustomerResponse toResponse(CustomerController customerController);
+    CustomerResponse toResponse(Customer customer);
 }
