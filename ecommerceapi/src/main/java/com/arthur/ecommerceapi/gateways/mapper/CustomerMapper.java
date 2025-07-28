7973eb87cab8321f3852938b5f0ea379c25e5e89
arthurd3 <arthur_camposl@yahoo.com>
2025-07-28 18:51:33 -0300

    adjust mapstruct

--- ecommerceapi/src/main/java/com/arthur/ecommerceapi/gateways/mapper/CustomerMapper.java
+++ ecommerceapi/src/main/java/com/arthur/ecommerceapi/gateways/mapper/CustomerMapper.java
@@ -1,16 +1,14 @@
 package com.arthur.ecommerceapi.gateways.mapper;
 
-import com.arthur.ecommerceapi.controllers.CustomerController;
 import com.arthur.ecommerceapi.domain.model.Customer;
 import com.arthur.ecommerceapi.dtos.request.CustomerRequest;
 import com.arthur.ecommerceapi.dtos.response.CustomerResponse;
 import com.arthur.ecommerceapi.gateways.entities.CustomerEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 
 import static org.mapstruct.ReportingPolicy.IGNORE;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING , unmappedTargetPolicy = IGNORE)
+@Mapper(componentModel = "spring" , unmappedTargetPolicy = IGNORE)
 public interface CustomerMapper {
 
     Customer toDomain(CustomerRequest dto);
