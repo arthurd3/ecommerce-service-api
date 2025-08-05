52a73f05f7a4a6636f5e38722f8e2a6b2692790f
arthurd3 <arthur_camposl@yahoo.com>
2025-08-05 11:01:07 -0300

    test: test validate customer

---
+++ ecommerceapi/src/test/java/com/arthur/ecommerceapi/customers/usecases/ValidatorCustomerTest.java
@@ -0,0 +1,54 @@
+package com.arthur.ecommerceapi.customers.usecases;
+
+import com.arthur.ecommerceapi.customers.domain.model.Customer;
+import com.arthur.ecommerceapi.customers.gateways.CustomerGateway;
+import com.arthur.ecommerceapi.testFactory.DataTestFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class ValidatorCustomerTest {
+
+    @InjectMocks
+    private ValidatorCustomer validator;
+
+    @Mock
+    private CustomerGateway customerGateway;
+
+
+
+    @Nested
+    class validateCustomer{
+
+        private Customer customer;
+
+        @BeforeEach
+        void setUp() {
+            customer = DataTestFactory.createCustomer();
+        }
+
+        @Test
+        @DisplayName("Should validate customer with success")
+        void shouldValidateCustomer(){
+
+            when(customerGateway.existsByEmail(customer.getEmail())).thenReturn(false);
+            when(customerGateway.existsByPhone(customer.getPhone())).thenReturn(false);
+
+            assertDoesNotThrow(() -> validator.validate(customer));
+            
+            verify(customerGateway , times(1)).existsByEmail(customer.getEmail());
+            verify(customerGateway , times(1)).existsByPhone(customer.getPhone());
+        }
+
+    }
+
+}
