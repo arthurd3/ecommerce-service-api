e8dd67c830ef61a1b2f775234d97c6c3d0bb1f7e
arthurd3 <arthur_camposl@yahoo.com>
2025-08-10 20:48:09 -0300

    obrigado por tudo na minha vida eu juro que vou vencar , obrigado por me mudar e me guiar para um caminho melhor na minha vida sou eternamente grato por tudo

--- ecommerceapi/src/test/java/com/arthur/ecommerceapi/orders/usecases/CreateOrderTest.java
+++ ecommerceapi/src/test/java/com/arthur/ecommerceapi/orders/usecases/CreateOrderTest.java
@@ -1,9 +1,13 @@
 package com.arthur.ecommerceapi.orders.usecases;
 
+import com.arthur.ecommerceapi.customers.gateways.entities.AddressEntity;
 import com.arthur.ecommerceapi.customers.gateways.entities.CustomerEntity;
 import com.arthur.ecommerceapi.orders.gateways.OrderSystemGateway;
 import com.arthur.ecommerceapi.orders.repositories.OrderRepository;
 import com.arthur.ecommerceapi.products.gateways.entities.ProductEntity;
+import com.arthur.ecommerceapi.testFactory.builders.AddressTestBuilder;
+import com.arthur.ecommerceapi.testFactory.builders.CustomerTestBuilder;
+import com.arthur.ecommerceapi.testFactory.builders.ProductTestBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,11 +32,13 @@ class CreateOrderTest {
 
     private CustomerEntity customerEntity;
     private ProductEntity productEntity;
+    private AddressEntity addressEntity;
 
     @BeforeEach
     void setUp() {
-
-
+        customerEntity = CustomerTestBuilder.aCustomer().buildEntity();
+        productEntity = ProductTestBuilder.aProduct().buildEntity();
+        addressEntity = AddressTestBuilder.aAddress().buildEntity();
     }
 
     @Test
