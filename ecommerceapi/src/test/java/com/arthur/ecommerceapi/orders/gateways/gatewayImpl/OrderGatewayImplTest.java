389a01e1415dab556002c007cc12a5b25f50c9d3
arthurd3 <arthur_camposl@yahoo.com>
2025-08-11 10:55:08 -0300

    test: create order with success test

---
+++ ecommerceapi/src/test/java/com/arthur/ecommerceapi/orders/gateways/gatewayImpl/OrderGatewayImplTest.java
@@ -0,0 +1,108 @@
+package com.arthur.ecommerceapi.orders.gateways.gatewayImpl;
+
+import com.arthur.ecommerceapi.customers.gateways.entities.AddressEntity;
+import com.arthur.ecommerceapi.customers.gateways.entities.CustomerEntity;
+import com.arthur.ecommerceapi.customers.repositories.CustomerRepository;
+import com.arthur.ecommerceapi.orders.enums.OrderStatus;
+import com.arthur.ecommerceapi.orders.gateways.OrderGateway;
+import com.arthur.ecommerceapi.orders.gateways.entities.OrderEntity;
+import com.arthur.ecommerceapi.orders.repositories.OrderRepository;
+import com.arthur.ecommerceapi.products.domain.models.Money;
+import com.arthur.ecommerceapi.products.domain.models.enums.ProductCategory;
+import com.arthur.ecommerceapi.products.gateways.entities.ProductEntity;
+import com.arthur.ecommerceapi.products.repositories.ProductRepository;
+import com.arthur.ecommerceapi.testFactory.builders.AddressTestBuilder;
+import com.arthur.ecommerceapi.testFactory.builders.CustomerTestBuilder;
+import com.arthur.ecommerceapi.testFactory.builders.ProductTestBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import jakarta.persistence.EntityManager;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class OrderGatewayImplTest {
+
+    @Autowired
+    private OrderGateway orderGateway;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+
+    @Nested
+    class createOrderWithSuccess{
+
+        @Test
+        @DisplayName("Should create order with success")
+        void shouldCreateOrderWithSuccess() {
+
+            CustomerEntity customerEntity = CustomerTestBuilder.aCustomer()
+                    .withName("John")
+                    .withEmail("john@gmail.com")
+                    .withPhone("123456789")
+                    .buildEntity();
+
+            ProductEntity productEntity = ProductTestBuilder.aProduct()
+                    .withName("Product 1")
+                    .withAvailableToDiscount(true)
+                    .withCategory(ProductCategory.ELECTRONICS)
+                    .withDescription("Product 1 from Electronics")
+                    .withQuantity(1000)
+                    .withPrice(new Money("199"))
+                    .buildEntity();
+
+            AddressEntity addressEntity = AddressTestBuilder.aAddress()
+                    .withStreet("123 Main St")
+                    .withCity("New York")
+                    .withCustomerEntity(customerEntity)
+                    .withCountry("Australia")
+                    .buildEntity();
+
+            OrderEntity orderExists = OrderEntity.createObj(
+                    productEntity ,
+                    customerEntity ,
+                    addressEntity ,
+                    "I Hate integration tests");
+
+
+            customerEntity.setAddress(addressEntity);
+            addressEntity.setCustomer(customerEntity);
+
+            customerRepository.save(customerEntity);
+            productRepository.save(productEntity);
+
+
+            OrderEntity savedOrder = orderRepository.save(orderExists);
+
+            entityManager.flush();
+
+            assertNotNull(savedOrder);
+            assertNotNull(savedOrder.getId());
+            assertEquals(OrderStatus.PENDING_PAYMENT, savedOrder.getStatus());
+            assertEquals(productEntity, savedOrder.getProduct());
+
+        }
+
+    }
+
+    @Test
+    void findById() {
+    }
+}
