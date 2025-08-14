66526ab3abe1fb856dbed217d59496a1cda9a5e7
arthurd3 <arthur_camposl@yahoo.com>
2025-08-14 12:53:07 -0300

    feat: adjust code for work all tests

---
+++ ecommerceapi/src/test/java/com/arthur/ecommerceapi/products/controllers/ProductControllerTest.java
@@ -0,0 +1,29 @@
+package com.arthur.ecommerceapi.products.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+}
