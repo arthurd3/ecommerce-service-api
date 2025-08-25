5b33ee570974718af9987f9c30ecc4389a001257
arthurd3 <arthur_camposl@yahoo.com>
2025-08-25 13:58:24 -0300

    feat: adjust on serialize and deserialize jwt tokens

--- ecommerceapi/src/main/java/com/arthur/ecommerceapi/security/jwt/TokenGeneration.java
+++ ecommerceapi/src/main/java/com/arthur/ecommerceapi/security/jwt/TokenGeneration.java
@@ -1,14 +1,12 @@
 package com.arthur.ecommerceapi.security.jwt;
 
 import com.arthur.ecommerceapi.customers.domain.model.Customer;
-import com.arthur.ecommerceapi.customers.domain.model.Role;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
-import java.util.stream.Collectors;
 
 @Component
 public class TokenGeneration {
